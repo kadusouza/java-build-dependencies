5f027da680331091df6655ddf22664e914fc8caa
Kadu Souza <kadu23977@gmail.com>
2023-06-17 16:23:04 -0300

    add multi-module project example

---
+++ maven/parent-app/controller/src/main/java/kadu/studies/App.java
@@ -0,0 +1,13 @@
+package kadu.studies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
